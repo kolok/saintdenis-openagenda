e6b79cf28ab2e57a5e9ccc116ffbef615c2a5032
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2021-09-01 09:59:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Saint-Denis OpenAgenda
+
+Portail et agenda des activités culturelles à Saint-Denis
+
+## Résumé
+
+## Solution technique
+
+Le portail est basé sur la technologie OpenAgenda et diffise les évènements aggrégés de l'agenda [Saint-Denis sur OpenAgenda](https://openagenda.com/saint-denis)
+
+La documentation suivie pour le développement de cette plateforme est disponible [ici](https://www.npmjs.com/package/@openagenda/agenda-portal)
+
+## Contact
+
+Pour toutes demandes ou réclamation, n'hésitez pas à me conacter : nicolas@oudard.org
---
+++ server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const log = require('@openagenda/agenda-portal/lib/Log')('server');
+const Portal = require('@openagenda/agenda-portal');
+
+Portal.utils.loadEnvironment(__dirname);
+
+function eventHook(event, lang, agenda, root) {
+  return event;
+}
+
+Portal({
+  root:
+    process.env.PORTAL_ROOT || `http://localhost:${process.env.PORTAL_PORT}`,
+  // agenda uid
+  uid: process.env.PORTAL_AGENDA_UID,
+  // site language
+  lang: process.env.PORTAL_LANG || 'fr',
+  // default timezone
+  defaultTimezone: process.env.PORTAL_DEFAULT_TIMEZONE || 'Europe/Paris',
+  // associated OA account key
+  key: process.env.PORTAL_KEY,
+  // views folder
+  views: `${__dirname}/views`,
+  // sass folder
+  sass: `${__dirname}/sass/main.scss`,
+  // assets folder
+  assets: `${__dirname}/assets`,
+  // multilingual labels folder
+  i18n: `${__dirname}/i18n`,
+  // number of events to be loaded in an event index page
+  eventsPerPage: 20,
+  // filter that applies when no other filter is specified
+  defaultFilter: {
+    featured: 0,
+  },
+  // true if portal is to be displayed within iframe
+  iframable: process.env.PORTAL_IFRAMABLE,
+  iframeParent: process.env.PORTAL_IFRAME_PARENT_URL,
+  cache: {
+    // interval at which cache is refreshed ( in milliseconds )
+    refreshInterval: 60 * 60 * 1000,
+  },
+  // map tiles
+  map: {
+    tiles: {
+      link: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+    auto: true,
+    /* center: {
+      latitude: 43.597198,
+      longitude: 1.441136
+    }, */
+    zoom: 20,
+  },
+  eventHook,
+}).then(({ app }) => app.launch(process.env.PORTAL_PORT));
